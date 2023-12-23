a672e71fb955cbb4fb586177ee511d865ac292a7
Dr. Christian Geuer-Pollmann <chgeuer@users.noreply.github.com>
2023-12-23 18:11:16 +0100

    Trying to bump to v0.1.1

--- checksum-Elixir.Windows.API.DataProtection.Native.exs
+++
@@ -1,4 +0,0 @@
-%{
-  "ex_windows_api_dataprotection-v0.1.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:e76cc5ef9d6c87cf69c856f6af453280914abb84b88b3fedfe354c8d1b28bba9",
-  "ex_windows_api_dataprotection-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:a97c048b9f0c2fdf3449889996b420984e22c3fe0573dae3a095582a8540c4e9",
-}
