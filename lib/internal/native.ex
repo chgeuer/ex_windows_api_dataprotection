580ebdd2b6d8d083c6129db25beca4edef18e57d
Dr. Christian Geuer-Pollmann <chgeuer@users.noreply.github.com>
2023-12-23 13:28:50 +0100

    Added more GitHub YAML magic from https://github.com/elixir-explorer/explorer/. I don't know what I'm doing 😬

--- lib/internal/native.ex
+++ lib/internal/native.ex
@@ -1,5 +1,32 @@
 defmodule Windows.API.DataProtection.Native do
-  use Rustler, otp_app: :ex_windows_api_dataprotection, crate: "ex_windows_api_dataprotection_rustler"
+  @moduledoc false
+
+  mix_config = Mix.Project.config()
+  version = mix_config[:version]
+  github_url = mix_config[:package][:links]["GitHub"]
+  mode = if Mix.env() in [:dev, :test], do: :debug, else: :release
+
+  # use Rustler,
+  #   otp_app: :ex_windows_api_dataprotection,
+  #   crate: "ex_windows_api_dataprotection_rustler"
+
+  use RustlerPrecompiled,
+    otp_app: :ex_windows_api_dataprotection,
+    version: version,
+    base_url: "#{github_url}/releases/download/v#{version}",
+    targets: ~w(
+      x86_64-pc-windows-msvc
+      x86_64-pc-windows-gnu
+    ),
+    variants: %{
+      "x86_64-pc-windows-msvc" => other_variants,
+      "x86_64-pc-windows-gnu" => other_variants,
+    },
+    # We don't use any features of newer NIF versions, so 2.15 is enough.
+    nif_versions: ["2.15"],
+    mode: mode
+    #force_build: System.get_env("EXPLORER_BUILD") in ["1", "true"]
+
 
   def nif_wrap(_cleartext_bytes), do: :erlang.nif_error(:nif_not_loaded)
 
