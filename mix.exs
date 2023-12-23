16cb1aa603e41775d405e97c0c94e681ba9fa679
Dr. Christian Geuer-Pollmann <chgeuer@users.noreply.github.com>
2023-12-23 13:17:30 +0100

    a

--- mix.exs
+++ mix.exs
@@ -1,13 +1,24 @@
 defmodule Windows.API.DataProtection.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/chgeuer/ex_windows_api_dataprotection"
+  @version "0.1.0"
+  @dev? String.ends_with?(@version, "-dev")
+  @force_build? System.get_env("EXPLORER_BUILD") in ["1", "true"]
+
   def project do
     [
       app: :ex_windows_api_dataprotection,
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: [
+        "rust.lint": ["cmd cargo clippy --manifest-path=native/ex_windows_api_dataprotection_rustler/Cargo.toml -- -Dwarnings"],
+        "rust.fmt": ["cmd cargo fmt --manifest-path=native/ex_windows_api_dataprotection_rustler/Cargo.toml --all"]
+        #"localstack.setup": ["cmd ./test/support/setup-localstack.sh"],
+        #ci: ["format", "rust.fmt", "rust.lint", "test"]
+      ]
     ]
   end
 
@@ -19,7 +30,9 @@ defmodule Windows.API.DataProtection.MixProject do
 
   defp deps do
     [
-       {:rustler, "~> 0.30.0", runtime: false}
+       {:rustler_precompiled, "~> 0.7"},
+       #{:rustler, "~> 0.30.0", runtime: false},
+       {:rustler, "~> 0.30.0", optional: not (@dev? or @force_build?)}
     ]
   end
 end
