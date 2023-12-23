052a31cb616197e7c1de40efd000160df2ae3cbe
Dr Christian Geuer-Pollmann <chgeuer@users.noreply.github.com>
2023-12-23 18:12:17 +0100

    Trying to bump to v0.1.1 (#1)

--- demo.exs
+++ demo.exs
@@ -1,7 +1,6 @@
 Mix.install([
-  {:ex_windows_api_dataprotection,
-    git: "https://github.com/chgeuer/ex_windows_api_dataprotection.git",
-    tag: "v0.1.0"}
+  # {:ex_windows_api_dataprotection, git: "https://github.com/chgeuer/ex_windows_api_dataprotection.git", tag: "v0.1.1"}
+  {:ex_windows_api_dataprotection, "~> 0.1.1"}
 ])
 
 import Windows.API.DataProtection, only: [wrap: 1, unwrap: 1]
